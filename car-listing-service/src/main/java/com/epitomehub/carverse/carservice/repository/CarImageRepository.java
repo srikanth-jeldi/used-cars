fc6e488027f6daf57cad64037513fa71cef23b81
srikanth jeldi <srikanth.jeldi@gmail.com>
2025-12-16 14:27:02 +0530

    added and updated all services

--- car-listing-service/src/main/java/com/epitomehub/carverse/carservice/repository/CarImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epitomehub.carverse.carservice.repository;
-
-import com.epitomehub.carverse.carservice.entity.CarImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarImageRepository extends JpaRepository<CarImage, Long> {
-}
--- car-listing-service/src/main/java/com/epitomehub/carverse/carservice/repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.epitomehub.carverse.carservice.repository;
-
-import com.epitomehub.carverse.carservice.entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CarRepository extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
-}
